38ef9e8cdba59b988948f41ec4881b1de0ebf082
Julian Grosshauser <julian@juliangrosshauser.com>
2015-04-22 23:15:14 +0200

    Add Xcode project

---
+++ Snoozify/Classes/Application/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Snoozify
+//
+//  Created by Julian Grosshauser on 22/04/15.
+//  Copyright (c) 2015 Julian Grosshauser. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        return true
+    }
+}
+
---
+++ SnoozifyTests/Classes/SnoozifyTests.swift
@@ -0,0 +1,25 @@
+//
+//  SnoozifyTests.swift
+//  SnoozifyTests
+//
+//  Created by Julian Grosshauser on 22/04/15.
+//  Copyright (c) 2015 Julian Grosshauser. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class SnoozifyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testExample() {
+        XCTAssert(true, "Pass")
+    }
+}
