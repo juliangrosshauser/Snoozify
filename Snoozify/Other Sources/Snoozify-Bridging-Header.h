d469a14c7f20113ba500351e822ef89767bd56a5
Julian Grosshauser <julian@juliangrosshauser.com>
2015-04-23 22:57:42 +0200

    Add Objective-C Bridging Header

---
+++ Snoozify/Other Sources/Snoozify-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  Snoozify-Bridging-Header.h
+//  Snoozify
+//
+//  Created by Julian Grosshauser on 23/04/15.
+//  Copyright (c) 2015 Julian Grosshauser. All rights reserved.
+//
+
+#import <Spotify/Spotify.h>
